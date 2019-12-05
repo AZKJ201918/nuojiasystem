df9b5fb4a4b1899622d5ff1d7133174bafb6bd4c
pc <1522628183@qqcom>
2019-12-05 18:06:18 +0800

    0000

---
+++ src/main/java/com/shopping/entity/Address.java
@@ -0,0 +1,103 @@
+package com.shopping.entity;
+
+public class Address {
+    private Integer id;
+
+    private String name;
+
+    private String phone;
+
+    private String country;
+
+    private String province;
+
+    private String city;
+
+    private String detail;
+
+    private String uid;
+
+    private Integer status;
+
+    private String area;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country == null ? null : country.trim();
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province == null ? null : province.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail == null ? null : detail.trim();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid == null ? null : uid.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+}
---
+++ src/main/java/com/shopping/mapper/AddressMapper.java
@@ -0,0 +1,20 @@
+package com.shopping.mapper;
+
+import com.shopping.entity.Address;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AddressMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Address record);
+
+    int insertSelective(Address record);
+
+    Address selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Address record);
+
+    int updateByPrimaryKey(Address record);
+}
